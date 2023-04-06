2345c715f13184dd1d61bb36cc9e17b0f6c089b6
MAsadAnwaar <98406037+MAsadAnwaar@users.noreply.github.com>
2023-04-06 14:54:45 +0500

    update

--- smsapp/admin.py
+++ smsapp/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
-
-# Register your models here.
-from django.contrib import admin
-from .models import category, sms,sub_category
+from .models import category, sms, sub_category
 
 admin.site.register(category)
 admin.site.register(sub_category)
-admin.site.register(sms)
+
+class SmsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'get_cat_name', 'sub_cat_name','sms')
+
+    def get_cat_name(self, obj):
+        return obj.sub_cat_name.cat_name
+    get_cat_name.short_description = 'Category'
+
+admin.site.register(sms, SmsAdmin)
--- smsapp/views.py
+++ smsapp/views.py
@@ -12,6 +12,28 @@ class CategoryList(generics.ListAPIView):
     queryset = category.objects.all()
     serializer_class = CategorySerializer
 
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset, many=True)
+        data = serializer.data
+
+        for category_data in data:
+            category_obj = category.objects.get(cat_name__iexact=category_data['cat_name'])  # Case-insensitive lookup
+            sub_cats = category_obj.sub_category_set.all()
+            sub_cat_serializer = SubCategorySerializer(sub_cats, many=True, context={'request': request})
+            category_data['sub_categories'] = sub_cat_serializer.data
+            category_data['cat_image_link'] = request.build_absolute_uri(category_obj.cat_image_link.url)
+
+            # serialize related sms for each sub_cat object
+            for sub_cat_data in category_data['sub_categories']:
+                sub_cat_obj = sub_category.objects.get(sub_cat_name__iexact=sub_cat_data['sub_cat_name'], cat_name=category_obj)
+                sms_objs = sms.objects.filter(sub_cat_name=sub_cat_obj)
+                sms_serializer = SmsSerializer(sms_objs, many=True, context={'request': request})
+                sub_cat_data['sms'] = sms_serializer.data
+
+        return Response(data)
+
+
 
 class SmsList(generics.ListAPIView):
     queryset = sms.objects.all()
